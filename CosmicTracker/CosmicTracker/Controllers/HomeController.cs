c48c91293a7221b54ec5338e033bc13849514ea5
Yevhen <ydombrovskyi@gmail.com>
2019-03-08 00:10:13 +0100

    With updated tracker

--- CosmicTracker/CosmicTracker/Controllers/HomeController.cs
+++ CosmicTracker/CosmicTracker/Controllers/HomeController.cs
@@ -2,45 +2,39 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Globalization;
-using System.Linq;
-using System.Net;
 using System.Net.Http;
-using System.Security.Policy;
 using System.Threading;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using CosmicTracker.Models;
-using Microsoft.CodeAnalysis;
 
 
 namespace CosmicTracker.Controllers
 {
     public class HomeController : Controller
     {
-       private double lat1;
-       private double lat2;
-       private double lon1;
-       private double lon2;
+        private double lat1;
+        private double lat2;
+        private double lon1;
+        private double lon2;
         List<string> datas = new List<string>();
         private string[] Peoples;
         public List<string> Team = new List<string>();
         public IActionResult Index()
         {
             Location();
-           var dis = Distance();
-            var Speedresult = Math.Round(dis/(TimeConverter()/3600),1); 
+            var dis = Distance();
+            var Speedresult = Math.Round(dis / (TimeConverter() / 3600), 1);
             var information = new List<string> {
-                Convert.ToString(lat1),
-                Convert.ToString(lon1),
-                Convert.ToString(lat2),
-                Convert.ToString(lon2),
+                Convert.ToString(datas[0]),
+                Convert.ToString(datas[1]),
+                Convert.ToString(datas[3]),
+                Convert.ToString(datas[4]),
                 Math.Round(dis,3).ToString(),
                 Speedresult.ToString(),
                 datas[6]
-               //// datas[7]//crew2
-                //datas[8]//crew3
+
             };
-          
+
             return View(information);
         }
         //  get Location 2 times
@@ -49,7 +43,7 @@ namespace CosmicTracker.Controllers
             HttpClient http = new HttpClient();
             try
             {
-                http.BaseAddress = new  Uri("http://api.open-notify.org/astros.json");
+                http.BaseAddress = new Uri("http://api.open-notify.org/astros.json");
                 http.Timeout = TimeSpan.FromSeconds(10);
             }
             catch (Exception)
@@ -72,10 +66,10 @@ namespace CosmicTracker.Controllers
                 }
             }
         }
-        public async void Location()
+        public void Location()
         {
             HttpClient http = new HttpClient();
-           
+
             try
             {
                 http.BaseAddress = new Uri("http://api.open-notify.org/iss-now.json");
@@ -97,10 +91,10 @@ namespace CosmicTracker.Controllers
             datas.Add(dat2.iss_position.latitude);
             datas.Add(dat2.iss_position.longitude);
             datas.Add(dat2.timestamp.ToString());
-           
-            
+
+
         }
-        
+
         //Main function to calculate distance   
         public double Distance()
         {
@@ -108,31 +102,31 @@ namespace CosmicTracker.Controllers
             lon1 = double.Parse(datas[1], CultureInfo.InvariantCulture);
             lat2 = double.Parse(datas[3], CultureInfo.InvariantCulture);
             lon2 = double.Parse(datas[4], CultureInfo.InvariantCulture);
-           
-                var d1 = lat1 * (Math.PI / 180.0);
-                var num1 = lon1 * (Math.PI / 180.0);
-                var d2 = lat2 * (Math.PI / 180.0);
-                var num2 = lon2 * (Math.PI / 180.0) - num1;
-                var d3 = Math.Pow(Math.Sin((d2 - d1) / 2.0), 2.0) + Math.Cos(d1) * Math.Cos(d2) * Math.Pow(Math.Sin(num2 / 2.0), 2.0);
-                var dist = 6371 * (2.0 * Math.Atan2(Math.Sqrt(d3), Math.Sqrt(1.0 - d3)));
+
+            var d1 = lat1 * (Math.PI / 180.0);
+            var num1 = lon1 * (Math.PI / 180.0);
+            var d2 = lat2 * (Math.PI / 180.0);
+            var num2 = lon2 * (Math.PI / 180.0) - num1;
+            var d3 = Math.Pow(Math.Sin((d2 - d1) / 2.0), 2.0) + Math.Cos(d1) * Math.Cos(d2) * Math.Pow(Math.Sin(num2 / 2.0), 2.0);
+            var dist = 6371 * (2.0 * Math.Atan2(Math.Sqrt(d3), Math.Sqrt(1.0 - d3)));
             return dist;
         }
 
         public double TimeConverter()
         {
-            
+
             var point1 = UnixTimeStampToDateTime(datas[2]);
             var point2 = UnixTimeStampToDateTime(datas[5]);
-            string format = "HH:mm  ddd d MMM yyyy"; 
+            string format = "HH:mm  ddd d MMM yyyy";
             datas.Add(point2.ToString(format));
-           var timeForMeasurm = (point2 - point1).TotalSeconds;
-           return timeForMeasurm;
+            var timeForMeasurm = (point2 - point1).TotalSeconds;
+            return timeForMeasurm;
         }
         //Converter unix Timestamp to Data
         public static DateTime UnixTimeStampToDateTime(string timeStamp)
         {
             // Unix timestamp is seconds past epoch
-           DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
             dtDateTime = dtDateTime.AddSeconds(Convert.ToDouble(timeStamp)).ToLocalTime();
             return dtDateTime;
         }
--- CosmicTracker/CosmicTracker/Models/Crew.cs
+++ CosmicTracker/CosmicTracker/Models/Crew.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace CosmicTracker.Models
+﻿namespace CosmicTracker.Models
 {
     public class Crew
     {
